6482b6b8f7e57de0c5c07e811456e1fc6cf52810
OrdunaGroup <121165740+OrdunaGroup@users.noreply.github.com>
2023-02-19 16:22:24 -0600

    Add files via upload

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,13 +14,34 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('user_id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->bigInteger('role_id')->default(2);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('auth_type')->nullable();
+            $table->longText('profile_image')->nullable();
+            $table->string('plan_id')->nullable();
+            $table->string('term')->nullable();
+            $table->longText('plan_details')->nullable();
+            $table->string('plan_validity')->nullable();
+            $table->timestamp('plan_activation_date')->nullable();
+            $table->string('billing_name')->nullable();
+            $table->string('type')->nullable();
+            $table->string('vat_number')->nullable();
+            $table->longText('billing_address')->nullable();
+            $table->string('billing_city')->nullable();
+            $table->string('billing_state')->nullable();
+            $table->string('billing_zipcode')->nullable();
+            $table->string('billing_country')->nullable();
+            $table->string('billing_phone')->nullable();
+            $table->string('billing_email')->nullable();
+            $table->integer('status')->default(1);
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
         });
     }
 
@@ -33,4 +54,4 @@ return new class extends Migration
     {
         Schema::dropIfExists('users');
     }
-};
+}
